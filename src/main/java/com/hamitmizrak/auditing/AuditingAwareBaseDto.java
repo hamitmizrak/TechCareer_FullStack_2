a020e610e691f52ec8a657dea8b23d54a6df5074
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-22 10:46:16 +0300

    AuditorAwareBean

--- src/main/java/com/hamitmizrak/auditing/AuditingAwareBaseDto.java
+++ src/main/java/com/hamitmizrak/auditing/AuditingAwareBaseDto.java
@@ -1,8 +1,14 @@
 package com.hamitmizrak.auditing;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -20,4 +26,20 @@ abstract  public class AuditingAwareBaseDto  implements Serializable {
     // DATE
     @Builder.Default
     private Date systemDate=new Date(System.currentTimeMillis());
+
+    // AUDTING
+    // @JsonIgnore => Backentte veri giderken bu bilgiyi gösterme
+    // KIM EKLEDİ ?
+    @JsonIgnore
+    protected String createdUser;
+
+    // KİM NE ZAMAN EKLEDİ ?
+    @JsonIgnore
+    protected Date createdDate;
+
+    // KIM GÜNCELLEDİ ?
+    protected String updatedUser;
+
+    // KİM NE ZAMAN GÜNCELLEDİ ?
+    protected Date updatedDate;
 }
---
+++ src/main/java/com/hamitmizrak/auditing/AuditorAwareBean.java
@@ -0,0 +1,14 @@
+package com.hamitmizrak.auditing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+@Configuration
+public class AuditorAwareBean {
+
+    @Bean
+    public AuditorAware<String> auditorAwareBean(){
+        return  new AuditorAwareImpl();
+    }
+}
