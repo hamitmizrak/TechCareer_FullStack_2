de85305bc36a3d2c5791bb34b301d7760b4a8901
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-28 13:33:43 +0300

    Main Link

--- frontend/src/component/Main.jsx
+++ frontend/src/component/Main.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Main extends Component {
   render() {
     return (
-      <div>Main</div>
+      <React.Fragment>
+        <h1 className="display-3">Ana Sayfa</h1>
+         <Link className="btn btn-primary mt-5 mb-5" to="category/list">Category - List</Link>
+      </React.Fragment>
     )
   }
 }
