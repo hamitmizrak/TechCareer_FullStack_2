87c0df25c15c40dd5d40da7a5008a331ba0a7239
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-28 14:41:18 +0300

    List

--- frontend/src/component/Main.jsx
+++ frontend/src/component/Main.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+// Resim
+import moon from "../img/moon.jpg"
+
 export default class Main extends Component {
   render() {
     return (
       <React.Fragment>
         <h1 className="display-3">Ana Sayfa</h1>
+        <img src={moon} alt="" style={{width:"100%"}} />
          <Link className="btn btn-primary mt-5 mb-5" to="category/list">Category - List</Link>
       </React.Fragment>
     )
--- frontend/src/component/category/CategoryList.jsx
+++ frontend/src/component/category/CategoryList.jsx
@@ -1,9 +1,109 @@
-import React, { Component } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
-export default class CategoryList extends Component {
-  render() {
-    return (
-      <div>CategoryList</div>
-    )
+// FUNCTION
+export default function CategoryList() {
+
+  // REDIRECT
+  let navigate = useNavigate();
+
+  // STATE
+  const [CategoryStateApi, setCategoryStateApi] = useState([]);
+
+  // USEEFFECT
+  useEffect(() => {
+    axios.get("http://localhost:4444/category/api/v1/list")
+      .then((response) => {
+        console.log(response.data);
+        setCategoryStateApi(response.data);
+      })
+      .catch((err) => { console.error(err); });
+  }, []);
+
+
+  // LIST
+  const getListCategory = (() => {
+    axios.get("http://localhost:4444/category/api/v1/list")
+      .then((response) => {
+        console.log(response.data);
+        setCategoryStateApi(response.data);
+      })
+      .catch((err) => { console.error(err); });
+  });
+
+  // DELETE
+  const setDeleteCategory = ((id) => {
+    if (window.confirm("Silmek istediğinizden emin misiniz ?")) {
+      axios.delete("http://localhost:4444/category/api/v1/delete/" + id)
+        .then(() => {
+          getListCategory();
+        })
+    } else {
+      alert("Silinmedi.")
+    }
+    navigate("/category/list");
+  });
+
+  //UPDATE
+  const setUpdateCategory = (data) => {
+    let { id, categoryName, systemDate } = data;
+    localStorage.setItem("category_update_id", id);
+    localStorage.setItem("category_update_category_name", categoryName);
+    localStorage.setItem("category_update_category_date", systemDate);
   }
+
+  //VIEW
+  const setViewCategory = (id) => {
+    localStorage.setItem("category_view_id", id);
+  }
+
+  //RETURN
+  return (
+    <React.Fragment>
+      <h1 className="text-center display-3">Category List</h1>
+      <Link to="/category/create" className="btn btn-primary">Category Ekle</Link>
+      <table className="table table-striped table-hover table-responsive">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>CATEGORY NAME</th>
+            <th>DATE</th>
+            <th>UPDATE</th>
+            <th>VIEW</th>
+            <th>DELETE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            CategoryStateApi.map((data) =>
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td>{data.categoryName}</td>
+                <td>{data.systemDate}</td>
+
+                <td>
+                  <Link to="/category/update">
+                  <i onClick={()=>setUpdateCategory(data)} class="fa-solid fa-pen-to-square text-primary"></i>
+                  </Link>
+                  </td>
+
+                <td>
+                <Link to="/category/view">
+                  <i onClick={()=>setViewCategory(data.id)} class="fa-solid fa-expand text-warning"></i>
+                  </Link>
+                  </td>
+
+                <td>
+                <Link to="/category/delete">
+                  <i onClick={()=>setDeleteCategory(data.id)} class="fa-solid fa-trash text-danger"></i>
+                  </Link>
+                  </td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </React.Fragment>
+  )
 }
